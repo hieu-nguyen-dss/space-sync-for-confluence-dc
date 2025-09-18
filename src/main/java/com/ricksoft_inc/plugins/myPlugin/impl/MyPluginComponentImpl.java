5b6c1a2d91b56619bc7e9fba7a0ca88fba744eea
Hieu Nguyen <hieu.nguyen@dssolution.jp>
2025-09-18 16:49:52 +0700

    init commit

---
+++ src/main/java/com/ricksoft_inc/plugins/myPlugin/impl/MyPluginComponentImpl.java
@@ -0,0 +1,25 @@
+package com.ricksoft_inc.plugins.myPlugin.impl;
+
+import com.atlassian.sal.api.ApplicationProperties;
+import com.ricksoft_inc.plugins.myPlugin.api.MyPluginComponent;
+
+
+public class MyPluginComponentImpl implements MyPluginComponent
+{
+        private final ApplicationProperties applicationProperties;
+
+        public MyPluginComponentImpl(final ApplicationProperties applicationProperties)
+    {
+        this.applicationProperties = applicationProperties;
+    }
+
+    public String getName()
+    {
+        if(null != applicationProperties)
+        {
+            return "myComponent:" + applicationProperties.getDisplayName();
+        }
+        
+        return "myComponent";
+    }
+}
